bace799b38c75b2ea45a5ea8d4195214d494ead7
CalebA-Codes <chrmcaleb@gmail.com>
2026-03-25 20:03:01 -0500

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    is_student = models.BooleanField(default=True)
     saved_money_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
