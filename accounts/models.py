d193fe044911564997df6c3e3c07a88404c01f8f
CalebA-Codes <chrmcaleb@gmail.com>
2026-03-25 20:03:30 -0500

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class CustomUser(AbstractUser):
     saved_money_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
